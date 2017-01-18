f60bf9e3a8b89d6e863dfc2bfb5ff5f0ae5d62d0
Michael J. Simons <michael@simons.ac>
2017-01-18 14:12:52 +0100

    Add example for explicitly listed property classes

---
+++ src/main/java/de/springbootbuch/extconfig/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package de.springbootbuch.extconfig;
+
+import de.springbootbuch.extconfig.ApplicationConfig.BarProperties;
+import de.springbootbuch.extconfig.ApplicationConfig.FooProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({BarProperties.class, FooProperties.class})
+public class ApplicationConfig {
+
+    @ConfigurationProperties("bar")
+    public class BarProperties {
+
+        private String otherValue;
+
+        public String getOtherValue() {
+            return otherValue;
+        }
+
+        public void setOtherValue(String otherValue) {
+            this.otherValue = otherValue;
+        }
+    }
+
+    @ConfigurationProperties("foo")
+    public static class FooProperties {
+
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+}
