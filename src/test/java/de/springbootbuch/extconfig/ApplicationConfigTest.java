5e07f03b50b9dce450a22bb743886544eac4d5de
Gerrit Meier <meistermeier@gmail.com>
2020-11-02 18:35:57 +0100

    Upgrade to Spring Boot 2.4.0-RC1.

--- src/test/java/de/springbootbuch/extconfig/ApplicationConfigTest.java
+++ src/test/java/de/springbootbuch/extconfig/ApplicationConfigTest.java
@@ -1,21 +1,20 @@
 package de.springbootbuch.extconfig;
 
+import static org.assertj.core.api.Assertions.*;
+
 import de.springbootbuch.extconfig.ApplicationConfig.BarService;
 import de.springbootbuch.extconfig.ApplicationConfig.FactoredService;
 import de.springbootbuch.extconfig.ApplicationConfig.FooService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestPropertySource(properties = "foo.value")
 public class ApplicationConfigTest {
@@ -26,22 +25,22 @@ public class ApplicationConfigTest {
 	@Test
 	public void checkUniqueServices() {
 		assertThat(
-			ctx.getBeansOfType(FooService.class).size(), 
-			is(equalTo(1)));
+			ctx.getBeansOfType(FooService.class))
+			.hasSize(1);
 		assertThat(
-			ctx.containsBean("myFooService"), 
-			is(true));
+			ctx.containsBean("myFooService"))
+			.isTrue();
 		assertThat(
-			ctx.getBeansOfType(BarService.class).size(),
-			is(equalTo(1)));
+			ctx.getBeansOfType(BarService.class))
+			.hasSize(1);
 		assertThat(
-			ctx.getBeansOfType(FactoredService.class).size(), 
-			is(equalTo(1)));
+			ctx.getBeansOfType(FactoredService.class))
+			.hasSize(1);
 
 		final BarService barService 
 			= ctx.getBean(BarService.class);
 		assertThat(
-			barService.fooService, 
-			is(equalTo(ctx.getBean(FooService.class))));
+			barService.fooService)
+			.isEqualTo(ctx.getBean(FooService.class));
 	}
 }
--- src/test/java/de/springbootbuch/extconfig/PlainConfigTest.java
+++ src/test/java/de/springbootbuch/extconfig/PlainConfigTest.java
@@ -1,16 +1,18 @@
 package de.springbootbuch.extconfig;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import de.springbootbuch.extconfig.PlainConfig.PlainPojo;
-import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PlainConfigTest {
 
